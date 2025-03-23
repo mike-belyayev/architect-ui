50093d73f4c3b2d1ccd4c79399d2f217e7fd2917
Mike Belyayev <4xgood@gmail.com>
2025-03-23 10:59:00 -0600

    addede clerk auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // App.tsx
 import React, { useState } from 'react';
 import './App.css';
+import { SignedIn, UserButton, SignInButton, SignedOut } from '@clerk/clerk-react';
 import FloorPlanCanvas from './FloorPlanCanvas';
 import Toolbox from './Toolbox';
 
 const App: React.FC = () => {
+
     const [selectedTool, setSelectedTool] = useState<string | null>(null);
 
     const handleToolSelect = (tool: string | null) => {
@@ -12,11 +14,21 @@ const App: React.FC = () => {
     };
 
     return (
-        <div className="App">
+            <div className="App">
+            <div style={{ position: 'absolute', top: 10, right: 10 }}>
+                <SignedOut>
+
+                    <SignInButton />
+                </SignedOut>
+                    <SignedIn>
+                    <UserButton /> 
+                    </SignedIn>
+            </div>
+
             <h1>Draw your floor plan</h1>
             <Toolbox onToolSelect={handleToolSelect} selectedTool={selectedTool} />
             <FloorPlanCanvas selectedTool={selectedTool} />
-        </div>
+            </div>
     );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
 import App from './App';
 import './index.css';
 import { MyContextProvider } from './MyContext';
+
+const clerkPubKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkPubKey) {
+  throw new Error("Missing Clerk Publishable Key");
+}
 
 const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <React.StrictMode>
+     <ClerkProvider publishableKey={clerkPubKey}>
     <MyContextProvider>
       <App />
     </MyContextProvider>
+     </ClerkProvider>
   </React.StrictMode>
 );
